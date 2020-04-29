208c8fadedfedf2870e042fe95dbabf859c893d9
Sina Khorami <sina_khorami@comp.iust.ac.ir>
2020-04-29 12:40:03 +0430

    fix: fixed argument parser

--- manage.py
+++ manage.py
@@ -1,33 +1,47 @@
+import os
+from dotenv import load_dotenv
+from argparse import ArgumentParser
+
+from app.util.enumeration import SourceType
+from app.process.twitter_stream_process import TwitterStreamProcess
+
+
+def run_twitter():
+    twitter_consumer_key = os.getenv('TWITTER_CONSUMER_KEY')
+    twitter_consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET')
+    twitter_access_token = os.getenv('TWITTER_ACCESS_TOKEN')
+    twitter_access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
+
+    process = TwitterStreamProcess(
+        consumer_key=twitter_consumer_key,
+        consumer_secret=twitter_consumer_secret,
+        access_token=twitter_access_token,
+        access_token_secret=twitter_access_token_secret
+    )
+
+    process.run()
+
+
+def run_instagram():
+    raise NotImplementedError
+
+
 if __name__ == '__main__':
-    import os
-    from dotenv import load_dotenv
-    from argparse import ArgumentParser
-
-
     load_dotenv()
+
     parser = ArgumentParser()
     parser.add_argument(
-        '-t',
-        '--twitter',
-        action='store_true',
-        help='gather twitter in stream mode'
+        '-s',
+        '--source',
+        required=True,
+        choices=SourceType.list(),
+        help='select source'
     )
+    
     args = parser.parse_args()
 
-    if args.twitter:
-        from app.process.twitter_stream_process import TwitterStreamProcess
-
-
-        twitter_consumer_key = os.getenv('TWITTER_CONSUMER_KEY')
-        twitter_consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET')
-        twitter_access_token = os.getenv('TWITTER_ACCESS_TOKEN')
-        twitter_access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
-
-        twitter_stream_process = TwitterStreamProcess(
-            consumer_key=twitter_consumer_key,
-            consumer_secret=twitter_consumer_secret,
-            access_token=twitter_access_token,
-            access_token_secret=twitter_access_token_secret
-        )
-        twitter_stream_process.run()
+    if args.source == SourceType.TWITTER.value:
+        run_twitter()
+    elif args.source == SourceType.INSTAGRAM.value:
+        run_instagram()
 
